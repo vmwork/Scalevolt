92270a66b20f70070540b4c41c8304d4c4a75d21
andreyeramte <andri.rraa@gmail.com>
2025-03-10 15:44:58 -0400

    feat: add all untracked files and update frontend assets

---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'scalevolt-store-main',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
---
+++ node_modules/@firebasegen/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'scalevolt-store-main',
+  location: 'us-central1'
+};
+
